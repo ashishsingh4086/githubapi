6271777472c245535af5b81b4d4d97d1493e3911
ashish.singh4086 <ashish.singh4086@outlook.com>
2017-08-06 15:36:04 -0400

    format

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,59 +1,63 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import User from './User';
 import UserInfo from './UserInfo';
-const URL= 'https://api.github.com/users/';
-const  client_id='cd276167eade15e7fe83';
-const  client_secret='031eba14400b8b2458afef88d7cb646ea62dc0cc';
+const URL = 'https://api.github.com/users/';
+const client_id = 'cd276167eade15e7fe83';
+const client_secret = '031eba14400b8b2458afef88d7cb646ea62dc0cc';
 
-class Main extends Component{
-constructor(props){
+class Main extends Component {
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       username: []
     }
-  
+
   }
 
-  getfolders=(e)=>{
-    let user= e.target.value;
-  
-    axios.get(`${URL}${user}?client_id=${client_id}&client_secret=${client_secret}`)
-    
-    .then ((response)=>{
+  getfolders = (e) => {
+    let user = e.target.value;
+    if (!e.target.value) {
       this.setState({
-        username: response.data
-      },()=>{
-          console.log(this.state.username);
+        username: []
       })
-    
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
+    }
+    axios.get(`${URL}${user}?client_id=${client_id}&client_secret=${client_secret}`)
+
+      .then((response) => {
+        this.setState({
+          username: response.data
+        }, () => {
+          console.log(this.state.username);
+        })
+
+      })
+      .catch((err) => {
+        console.log(err);
+      })
 
   }
   render() {
     return (
       <div>
         <div className="container">
-            <input className="form-control" onChange={this.getfolders} />
-     
-            <User value ={this.state.username} />
-            <UserInfo value={this.state.username}/>
+          <input className="form-control" onChange={this.getfolders} />
 
-         </div>
-    </div>  
+          <User value={this.state.username} />
+          <UserInfo value={this.state.username} />
+
+        </div>
+      </div>
     );
   }
 }
 
-export default Main;         
-                  
-         
-               
-            
-        
-  
+export default Main;
 
-   
+
+
+
+
+
+
+
